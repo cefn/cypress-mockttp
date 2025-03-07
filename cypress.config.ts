ba95cce1bddcba3050c82eba4b83b5e9f949bd7e
Cefn Hoile <choile@roku.com>
2025-03-07 23:09:59 +0000

    Passing test after webpack overrides of node modules

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,50 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
+import webpack from "webpack"
+import webpackPreprocessor from "@cypress/webpack-preprocessor";
 
 export default defineConfig({
-  fixturesFolder: false,
-  screenshotsFolder: 'screenshots',
-  videosFolder: 'videos',
   e2e: {
-    setupNodeEvents(on, config) {},
     specPattern: './**/*.spec.ts',
     supportFile: false,
+    setupNodeEvents(on, config) {
+      const webpackOptions = {
+        resolve: {
+          extensions: [".ts", ".js", ".wasm"],
+          fallback: {
+            "stream": require.resolve("stream-browserify"),
+            "zlib": false, 
+            "querystring": false, 
+            "path": false, 
+            "fs": false 
+          } as const
+        },
+        module: {
+          rules: [
+            {
+              test: /\.wasm$/,
+              type: "asset/resource", // Ensures WebAssembly files are handled correctly
+            },
+          ],
+        },
+        experiments: {
+          asyncWebAssembly: true, // Enable WebAssembly
+          syncWebAssembly: true,  // (Optional) Enable synchronous WASM
+        },
+        plugins: [
+          new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+            process: "process/browser",
+          }),
+        ]
+      };
+
+      const options = {
+        webpackOptions,
+      };
+
+      on("file:preprocessor", webpackPreprocessor(options));
+
+      return config;
+    },
   },
-})
+});
